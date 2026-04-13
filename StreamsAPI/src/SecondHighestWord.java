a153bae28a1c9b09e1a41dd33075c6840da603b6
manjuram07 <khmanjunatha405@gmail.com>
2026-04-13 14:04:11 +0530

    Second Highest word

--- StreamsAPI/src/SecondHighestWord.java
+++ StreamsAPI/src/SecondHighestWord.java
@@ -7,12 +7,12 @@ public class SecondHighestWord {
 
         String words = "I am learning stream api in java";
 
-        Optional<String> first = Arrays.stream(words.split("\\s+"))
+        Optional<String> second = Arrays.stream(words.split("\\s+"))
                                         .sorted((o1, o2) -> Integer.compare(o2.length(), o1.length()))
                                         .skip(1)
                                         .findFirst();
 
-        System.out.println(first.get());
+        second.ifPresent(word -> System.out.println(word));
 
 
     }
