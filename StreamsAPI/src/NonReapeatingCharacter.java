6776da93acc6a4f61b342b50b89cceac82429d29
manjuram07 <khmanjunatha405@gmail.com>
2026-04-14 14:14:52 +0530

    find the first non-repeating character.

--- StreamsAPI/src/NonReapeatingCharacter.java
+++ StreamsAPI/src/NonReapeatingCharacter.java
@@ -1,11 +1,11 @@
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class NonReapeatingCharacter {
 
     /*
-    Given a string, find the first non-repeating character.
+        Given a string, find the first non-repeating character.
         input: "hello world"
         output: "h"
 
@@ -16,18 +16,13 @@ public class NonReapeatingCharacter {
 
         String str = "hello world";
 
-        Map<Character, Integer> map = new LinkedHashMap<>();
+        String s = Arrays.stream(str.split(""))
+                .filter(ch -> str.indexOf(ch) == str.lastIndexOf(ch))
+                .findFirst()
+                .get();
 
-        for (int i=0; i<str.length(); i++){
-            map.put(str.charAt(i), map.getOrDefault(str.charAt(i),0)+1);
-        }
+        System.out.println(s);
 
-        for (Map.Entry<Character, Integer> entry : map.entrySet()){
 
-           if (entry.getValue() == 1) {
-               System.out.println(entry.getKey());
-               break;
-           }
-        }
     }
 }
