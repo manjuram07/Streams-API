bcdd47bbb5cf41d54f7958bf938e548e2ff7db2d
manjuram07 <khmanjunatha405@gmail.com>
2026-04-14 14:41:23 +0530

    find the first non-repeating character.

--- StreamsAPI/src/NonReapeatingCharacter.java
+++ StreamsAPI/src/NonReapeatingCharacter.java
@@ -1,27 +1,41 @@
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NonReapeatingCharacter {
 
     /*
         Given a string, find the first non-repeating character.
-        input: "hello world"
-        output: "h"
+        input: "hello world"        input: "abcabcde"
+        output: "h"                 output: "d"
 
-        input: "abcabcde"
-        output: "d"
      */
     public static void main(String[] args) {
 
-        String str = "hello world";
-
+        String str = "abcabcde";
+/*
         String s = Arrays.stream(str.split(""))
                 .filter(ch -> str.indexOf(ch) == str.lastIndexOf(ch))
                 .findFirst()
                 .get();
 
         System.out.println(s);
+
+ */
+
+        LinkedHashMap<Character, Long> collect = str.chars().mapToObj(ch -> (char) ch)
+                                                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+
+        System.out.println(collect);
+
+        Character c = collect.entrySet()
+                            .stream().filter(map -> map.getValue() == 1)
+                            .map(m -> m.getKey())
+                            .findFirst()
+                            .get();
+
+        System.out.println(c);
 
 
     }
