413a546f5da238393cc0db7bac6da092bce30eb0
manjuram07 <khmanjunatha405@gmail.com>
2026-05-16 13:11:38 +0530

    Non repeating characters

--- StreamsAPI/src/NonReapeatingCharacter.java
+++ StreamsAPI/src/NonReapeatingCharacter.java
@@ -13,30 +13,16 @@ public class NonReapeatingCharacter {
      */
     public static void main(String[] args) {
 
-        String str = "abcabcde";
-/*
-        String s = Arrays.stream(str.split(""))
-                .filter(ch -> str.indexOf(ch) == str.lastIndexOf(ch))
-                .findFirst()
-                .get();
+        String str = "hello world";
 
-        System.out.println(s);
+        Map<Character, Long> collect = str.chars()
+                                            .mapToObj(ch -> (char) ch)
+                                            .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap:: new, Collectors.counting()));
 
- */
+        Optional<Character> characterStream = collect.entrySet()
+                                                    .stream().filter(value -> value.getValue() == 1)
+                                                    .map(key -> key.getKey()).findFirst();
 
-        LinkedHashMap<Character, Long> collect = str.chars().mapToObj(ch -> (char) ch)
-                                                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
-
-        System.out.println(collect);
-
-        Character c = collect.entrySet()
-                            .stream().filter(map -> map.getValue() == 1)
-                            .map(m -> m.getKey())
-                            .findFirst()
-                            .get();
-
-        System.out.println(c);
-
-
+        System.out.println(characterStream.get());
     }
 }
